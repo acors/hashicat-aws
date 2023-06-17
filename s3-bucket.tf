6bd8a7c619fc80ad55f16c9b00f32c4846b0f92d
Oscar Madrigal <oscar.madrigal.valdes@gmail.com>
2023-06-17 20:07:43 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
-  bucket_prefix = "${var.prefix}"
+  bucket = "${var.prefix}-my-s3-bucket"
   acl    = "private"
 
   control_object_ownership = true
